4b72f7e4e81dd50d0f476c6ecdf0edabf368684b
Kurt Duncan <kurtaduncan@gmail.com>
2023-10-28 11:44:02 -0600

    Add boiler-plate main code
    ... and .gitignore

--- src/com/bearsnake/ttt/Main.java
+++ src/com/bearsnake/ttt/Main.java
@@ -1,4 +1,25 @@
 package com.bearsnake.ttt;
 
 public class Main {
+
+    public static void main(
+        String[] args
+    ) {
+        Grid g = new Grid();
+        Player[] players = new Player[]{
+            new Player("Player 1", 'X'),
+            new Player("Player 2", 'O'),
+        };
+
+        Player turn = players[0];
+        while (!g.isGameOver()) {
+            //  TODO stuffs
+        }
+
+        if (g.whoWon() == null) {
+            //  TODO
+        } else {
+            //  TODO
+        }
+    }
 }
